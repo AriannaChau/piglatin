031b019b845bce99e109aca76f3df0d8fed76d2f
Arianna Chau and Steven Galvin <arianna@>
2017-03-08 19:57:09 -0800

    add js and jquery to grab input string and convert to array of individual characters.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
+var vowels = ["a", "e", "i", "o", "u"]
+
+
 $(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-  }
-}
+
+    var translation = $("#english").val();
+
+    var res = translation.split("");
+    console.log(res);
+
+    $(".translation").text(translation);
+    $("#output").show();
+  });
+});
